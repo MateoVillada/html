1570b91292124d1e6e80135ca3f7ff2fd2563735
MateoVillada <mateovillada26@gmail.com>
2023-11-21 22:58:41 -0500

    script 2.0

--- script.js
+++ script.js
@@ -21,3 +21,27 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = 'login.html';
     });
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const userMenu = document.getElementById('userMenu');
+
+    // Verificar si el usuario está autenticado
+    const authenticatedUser = localStorage.getItem('firstName');
+    
+    if (authenticatedUser) {
+        // Mostrar el nombre del usuario y opción para cerrar sesión
+        userMenu.innerHTML = `
+            <li>Bienvenido, ${authenticatedUser}</li>
+            <li><a href="#" id="logout">Cerrar Sesión</a></li>
+        `;
+
+        // Manejar evento de cierre de sesión
+        const logoutButton = document.getElementById('logout');
+        logoutButton.addEventListener('click', function () {
+            // Limpiar localStorage al cerrar sesión (esto debería ser manejado por el servidor en un entorno real)
+            localStorage.clear();
+            // Redirigir a la página de inicio
+            window.location.href = 'index.html';
+        });
+    }
+});
+
